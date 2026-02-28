ca4bc7dd444f1eec14c586cf31ee7a69b251a929
glaizamaeretardo <85205695+Eirosan29@users.noreply.github.com>
2026-02-28 11:42:55 +0800

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace TaskStatusProgram
+{
+    class Program
+    {
+        static string[] tasks = new string[100];
+        static int taskCount = 0;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Task Status Program");
+                Console.WriteLine("1. Add Task");
+                Console.WriteLine("2. View Tasks");
+                Console.WriteLine("3. Exit");
+
+                Console.Write("Choose an option: ");
+                string option = Console.ReadLine();
+
+                if (option == "1")
+                {
+                    addTask();
+                }
+
+                else if (option == "2")
+                {
+                    addView();
+                }
+
+                else if (option == "3")
+                {
+                    Console.WriteLine("Thank you for using the code:)) ");
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Please only input what on the menu, thank you:))");
+                }
+
+                Console.WriteLine();
+            }
+        }
+        static void addTask()
+        {
+            Console.Write("Enter Here: ");
+            string task = Console.ReadLine();
+
+            tasks[taskCount] = task;
+            taskCount++;
+
+            Console.WriteLine("Task Added:)) ");
+        }
+        static void addView()
+        {
+            Console.WriteLine("Here's the task/s: ");
+            for (int i = 0; i < taskCount; i++)
+            {
+                Console.WriteLine("* " + tasks[i]);
+            }
+        }
+    }
+}
