b199b1e1d233e983e7a2b5f293d60df30a2d51c5
Sam545 <micheal.samiayt@gmail.com>
2026-03-12 21:11:46 +0800

    made a class library called TaskAppService that holds a funtion

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;   
+using System.Collections.Generic;
+using TaskAppService;
 
 namespace TaskStatusProgram
 {
     class Program
     {
-       static List<string> tasks = new List<string>();  
-
         static void Main(string[] args)
         {
+            List<string> tasks = new List<string>();
+
+            TaskAppService.TaskAppService taskApp = new TaskAppService.TaskAppService();
+            taskApp.addTask(tasks);
+
             while (true)
             {
                 Console.WriteLine("--- Task Status Program ---");
@@ -26,24 +30,24 @@ namespace TaskStatusProgram
 
                 if (option == "1")
                 {
-                    addTask();
+                    addTask(tasks);
                 }
 
                 else if (option == "2")
                 {
-                    addView();
+                    addView(tasks);
                 }
                 else if (option == "3")
                 {
-                    addEdit();
+                    addEdit(tasks);
                 }
                 else if (option == "4")
                 {
-                    addDelete();
+                    addDelete(tasks);
                 }
                 else if (option == "5")
                 {
-                    addMarkTask();
+                    addMarkTask(tasks);
                 }
                 else if (option == "6")
                 {
@@ -58,74 +62,74 @@ namespace TaskStatusProgram
                 Console.WriteLine();
             }
         }
-        static void addTask()
+        //remove this function since it's redundant with taskAppService class(the one we want to use)
+        static void addTask(List<string>tasks)
         {
             Console.Write("Enter Here: ");
             string task = Console.ReadLine();
 
-            tasks[taskCount] = task;
-            taskCount++;
+           tasks.Add(task);  
 
             Console.WriteLine("Task Added:)) ");
 
             Console.WriteLine("----------------------");
         }
-        static void addView()
+        static void addView(List<string>tasks)
         {
             Console.WriteLine("---- Here's the task/s ----");
-            for (int i = 0; i < taskCount; i++)
+            for (int i = 0; i < tasks.Count; i++)
             {
-                Console.WriteLine("* " + tasks[i]);
+                Console.WriteLine((i + 1) + ". " + tasks[i]);
             }
 
             Console.WriteLine("----------------------");
         }
-        static void addEdit()
+        static void addEdit(List<string> tasks)
         {
             Console.WriteLine();
-            addView();
+            addView(tasks);
             Console.WriteLine();
 
-            Console.write("Enter the number of the task you want to edit: ");   
+            Console.Write("Enter the number of the task you want to edit: ");
             int editNum = Convert.ToInt32(Console.ReadLine());
-            
+
             Console.Write("Enter the new task: ");
             string newTask = Console.ReadLine();
 
-            tasks[editNum - 1] = newTask;   
+            tasks[editNum - 1] = newTask;
 
             Console.WriteLine("Task Edited:)) ");
         }
-        static void addDelete()
+        static void addDelete(List<string> tasks)
         {
             Console.WriteLine();
-            addView();
+            addView(tasks);
             Console.WriteLine();
 
-            Console.write("Enter the number of the task you want to edit: ");
+            Console.Write("Enter the number of the task you want to edit: ");
             int deleteNum = Convert.ToInt32(Console.ReadLine());
 
             //tasks[deleteNum - 1];
 
-            tasks.RemoveAt[deleteNum - 1];  
+            tasks.RemoveAt(deleteNum - 1);
 
-            Console.WriteLine("Task Deleted:)) ");  
+            Console.WriteLine("Task Deleted:)) ");
         }
-        static void addMarkTask()
+        static void addMarkTask(List<string> tasks)
         {
             Console.WriteLine();
-            addView();
+            addView(tasks);
             Console.WriteLine();
 
-            Console.write("Enter the number of the task you want to edit: ");
-            int deleteNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number of the task you want to mark: ");
+            int markNum = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Enter status (Done/Not Done): ");
             string status = Console.ReadLine();
 
-            tasks[deleteNum - 1] = tasks[deleteNum - 1] + " - " + status;
+            tasks[markNum - 1] = tasks[markNum - 1] + " [" + status + "]";
 
-            Console.WriteLine("Task Marked:)) ");   
+            Console.WriteLine("Task Marked:)) ");
         }
         static void addExit()
         {
---
+++ TaskAppService/TaskAppService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace TaskAppService
+{
+    public class TaskAppService
+    {
+
+        public void addTask(List<string> tasks)
+        {
+            Console.Write("Enter Here: ");
+            string task = Console.ReadLine();
+
+            tasks.Add(task);
+
+            Console.WriteLine("Task Added:)) ");
+
+            Console.WriteLine("----------------------");
+        }
+
+
+    }
+}
