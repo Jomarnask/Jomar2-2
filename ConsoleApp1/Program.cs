f74b9bcc99eacf58ddb2b680c87f8081874d67dd
Jomarnask <jomarnask@gmail.com>
2026-03-26 15:36:55 +0800

    newly added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,6 @@ namespace TaskStatusProgram
         static void Main(string[] args)
         {
             List<string> tasks = new List<string>();
-
             TaskAppService.TaskAppService taskApp = new TaskAppService.TaskAppService();
 
             while (true)
@@ -21,57 +20,92 @@ namespace TaskStatusProgram
                 Console.WriteLine("4. Delete Tasks");
                 Console.WriteLine("5. Mark Tasks");
                 Console.WriteLine("6. Exit");
-
                 Console.WriteLine();
-
                 Console.Write("Choose an option: ");
                 string option = Console.ReadLine();
 
                 if (option == "1")
                 {
-                    taskApp.addTask(tasks);
+                    Console.Write("Enter Here: ");
+                    string task = Console.ReadLine();
+                    taskApp.addTask(tasks, task);
+                    Console.WriteLine("Task Added:)) ");
+                    Console.WriteLine("------------------------");
                 }
-
                 else if (option == "2")
                 {
-                    taskApp.addView(tasks);
+                    Console.WriteLine("----- Here's the task/s -----");
+                    for (int i = 0; i < tasks.Count; i++)
+                        Console.WriteLine((i + 1) + ". " + tasks[i]);
+                    Console.WriteLine("------------------------");
                 }
                 else if (option == "3")
                 {
-                    taskApp.addEdit(tasks);
+                    Console.WriteLine("----- Here's the task/s -----");
+                    for (int i = 0; i < tasks.Count; i++)
+                        Console.WriteLine((i + 1) + ". " + tasks[i]);
+                    Console.WriteLine("------------------------");
+
+                    Console.Write("Enter the number of the task you want to edit: ");
+                    int editNum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter the new task: ");
+                    string newTask = Console.ReadLine();
+
+                    taskApp.addEdit(tasks, editNum - 1, newTask);
+                    Console.WriteLine("Task Edited:)) ");
+                    Console.WriteLine("------------------------");
                 }
                 else if (option == "4")
                 {
-                    taskApp.addDelete(tasks);
+                    Console.WriteLine("----- Here's the task/s -----");
+                    for (int i = 0; i < tasks.Count; i++)
+                        Console.WriteLine((i + 1) + ". " + tasks[i]);
+                    Console.WriteLine("------------------------");
+
+                    Console.Write("Enter the number of the task you want to delete: ");
+                    int deleteNum = Convert.ToInt32(Console.ReadLine());
+
+                    taskApp.addDelete(tasks, deleteNum - 1);
+                    Console.WriteLine("Task Deleted:)) ");
+                    Console.WriteLine("------------------------");
                 }
                 else if (option == "5")
                 {
-                    taskApp.addMarkTask(tasks);
+                    Console.WriteLine("----- Here's the task/s -----");
+                    for (int i = 0; i < tasks.Count; i++)
+                        Console.WriteLine((i + 1) + ". " + tasks[i]);
+                    Console.WriteLine("------------------------");
+
+                    Console.Write("Enter the number of the task you want to mark: ");
+                    int markNum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter status (Done/Not Done): ");
+                    string status = Console.ReadLine();
+
+                    if (status != "Done" && status != "Not Done")
+                    {
+                        Console.WriteLine("Invalid status! Please enter Done or Not Done.");
+                    }
+                    else
+                    {
+                        taskApp.addMarkTask(tasks, markNum - 1, status);
+                        Console.WriteLine("Task Marked:)) ");
+                    }
+
+                    Console.WriteLine("------------------------");
                 }
                 else if (option == "6")
                 {
-                    addExit();
+                    Console.WriteLine("Goodbye!");
+                    break;
                 }
-
                 else
                 {
-                    addError();
+                    Console.WriteLine("Invalid option. Try again.");
+                    Console.WriteLine("------------------------");
                 }
-
-                Console.WriteLine();
             }
-        }
-        //remove this function since it's redundant with taskAppService class(the one we want to use)
-        static void addExit()
-        {
-            Console.WriteLine("Thank you for using the code:)) ");
-
-            Console.WriteLine("----------------------");
-        }
-
-        static void addError()
-        {
-            Console.WriteLine("Please only input what on the menu, thank you:))");
         }
     }
 }
--- TaskAppService/TaskAppService.cs
+++ TaskAppService/TaskAppService.cs
@@ -1,79 +1,39 @@
-﻿using System.Threading.Tasks;
-
-namespace TaskAppService
+﻿namespace TaskAppService
 {
     public class TaskAppService
     {
-
-        public void addTask(List<string> tasks)
+        public void addTask(List<string> tasks, string task)
         {
-            Console.Write("Enter Here: ");
-            string task = Console.ReadLine();
-
-            tasks.Add(task);
-
-            Console.WriteLine("Task Added:)) ");
-
-            Console.WriteLine("----------------------");
+            tasks.Add(task + " [Not Done]");  
         }
+
         public void addView(List<string> tasks)
         {
-            Console.WriteLine("---- Here's the task/s ----");
-            for (int i = 0; i < tasks.Count; i++)
+
+        }
+
+        public void addEdit(List<string> tasks, int index, string newTask)
+        {
+            if (index >= 0 && index < tasks.Count)
+                tasks[index] = newTask;
+        }
+
+        public void addDelete(List<string> tasks, int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+                tasks.RemoveAt(index);
+        }
+
+        public void addMarkTask(List<string> tasks, int index, string status)
+        {
+            if (index >= 0 && index < tasks.Count)
             {
-                Console.WriteLine((i + 1) + ". " + tasks[i]);
+                int bracketIndex = tasks[index].IndexOf(" [");
+                if (bracketIndex >= 0)
+                    tasks[index] = tasks[index].Substring(0, bracketIndex);
+
+                tasks[index] = tasks[index] + " [" + status + "]";
             }
-
-            Console.WriteLine("----------------------");
         }
-        public void addEdit(List<string> tasks)
-        {
-            Console.WriteLine();
-            addView(tasks);
-            Console.WriteLine();
-
-            Console.Write("Enter the number of the task you want to edit: ");
-            int editNum = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter the new task: ");
-            string newTask = Console.ReadLine();
-
-            tasks[editNum - 1] = newTask;
-
-            Console.WriteLine("Task Edited:)) ");
-        }
-        public void addDelete(List<string> tasks)
-        {
-            Console.WriteLine();
-            addView(tasks);
-            Console.WriteLine();
-
-            Console.Write("Enter the number of the task you want to edit: ");
-            int deleteNum = Convert.ToInt32(Console.ReadLine());
-
-            //tasks[deleteNum - 1];
-
-            tasks.RemoveAt(deleteNum - 1);
-
-            Console.WriteLine("Task Deleted:)) ");
-        }
-        public void addMarkTask(List<string> tasks)
-        {
-            Console.WriteLine();
-            addView(tasks);
-            Console.WriteLine();
-
-            Console.Write("Enter the number of the task you want to mark: ");
-            int markNum = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter status (Done/Not Done): ");
-            string status = Console.ReadLine();
-
-            tasks[markNum - 1] = tasks[markNum - 1] + " [" + status + "]";
-
-            Console.WriteLine("Task Marked:)) ");
-        }
-
-
     }
 }
